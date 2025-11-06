73b26a3ec60422230bdcee32e7aea6a4131db84c
Alicia Severiano Perez <144361527+aseveria@users.noreply.github.com>
2025-11-05 19:20:52 -0600

    pushing of data

--- PowerPlants/server.R
+++ PowerPlants/server.R
@@ -21,12 +21,27 @@ server = function(input, output, session){
     
   })
   
+  
+  map_lp = reactive({
+    
+    mn_powerplants %>%
+      filter(first_op_year < as.numeric(input$numeric[1]))%>%
+      filter(!is.na(Longitude) & !is.na(Latitude)) %>%
+      group_by(plant_code, Longitude,Latitude) %>%
+      summarize (count = n()) %>%
+      st_as_sf(coords = c('Longitude', 'Latitude')) %>%
+      st_set_crs(4326)
+    
+  })
+  
   output$map = renderLeaflet({
     
     leaflet() %>%
       addTiles() %>%
       setView(lng = -94.6, lat = 46.4, zoom = 6) %>%
-      addCircleMarkers(data = map_df())
+      addCircleMarkers(data = map_df(), color = "red", radius = 3) %>%
+      addCircleMarkers(data = map_lp(), color = "grey", radius = 1)
+    
     
   })
   
